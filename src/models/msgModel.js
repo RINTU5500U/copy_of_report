fc9f1f566b3a079d869d93afcf33391567173d37
RINTU5500U <crowdrintu1@gmail.com>
2023-03-18 18:01:20 +0530

    done

---
+++ src/models/msgModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const msgSchema = new mongoose.Schema({
+    subject: {
+        type: String,
+        trim: true
+    },
+    helpMsg: {
+        type: String,
+            trim: true
+    },
+    file: {
+        type: String,
+        trim: true
+    },
+    contact: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    request: {
+        type: String,
+        enum: ['low', 'medium', 'high'],
+        required: true
+    },
+    createdAt: { 
+        type: String,
+        default: new Date().toLocaleString()
+    }
+});
+
+module.exports = mongoose.model('Msg', msgSchema) 
---
+++ src/routes/route.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+const msgModel = require('../models/msgModel')
+const AWS = require("aws-sdk");
+AWS.config.update({
+    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
+    secretAccessKey: "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",
+    region: "ap-south-1",
+});
+const s3 = new AWS.S3({ appVersion: "2006-03-01" });
+
+
+router.post('/sendMessage', async (req, res) => {
+    try {
+        const file = req.files[0]
+        const {subject, helpMsg, contact, request} = req.body
+        const uploadedFile = async (file) => {
+            return new Promise(function (resolve, reject) {
+                const { originalname, buffer } = file
+                const uploadParams = {
+                    Bucket: "classroom-training-bucket",
+                    Key: `biswajit-aws/${originalname}`,
+                    Body: buffer,
+                };
+
+                s3.upload(uploadParams, (err, data) => {
+                    if (err) {
+                        return reject({ error: err });
+                    } else {
+                        return resolve(data.Location);
+                    }
+                    });
+                });
+            };
+
+            uploadedFile(file).then( async (data) => {
+                req.body['file'] = data
+                let saveData = await msgModel.create(req.body)
+                return res.status(201).send({ status: true, msg: 'msg sending successfully', Data: saveData})
+            }).catch((err)=>{
+                console.log(err)
+                res.status(400).send(err)
+            })
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+router.all("/*", (req, res) => {
+    return res.status(400).send({ status: false, msg: "end point is not valid" });
+});
+  
+module.exports = router;
